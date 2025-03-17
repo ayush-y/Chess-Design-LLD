38020d80b39cbaedbce347f4bab924d1395e2a68
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2025-03-17 23:12:58 +0530

    Added cell

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import modles.Board.Cell;
 import modles.Helpers.Color;
 import modles.Helpers.Pair;
 
@@ -7,11 +8,14 @@ public class Main {
         Pair pair = new Pair(1, 2);
         System.out.println(pair.getX());
         System.out.println(pair.getY());
-        System.out.println("Thank you for playing the Chess !!");
         Color color = Color.BLACK;
         Color color1  = Color.WHITE;
         System.out.println(color);
         System.out.println(color1);
-        
+        Cell cell = new Cell(1, 2);
+        System.out.println(cell.getX());
+        System.out.println(cell.getY());
+        System.out.println("Thank you for playing the Chess !!");
+
     }
 }
